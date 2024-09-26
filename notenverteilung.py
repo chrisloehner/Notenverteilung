f05a5028ad17748b133a02b1767db5629b8fb021
ChrisLoehner <c.loehner410@gmail.com>
2024-09-26 18:49:54 +0200

    Öffnen der Datei hinzugefügt

--- notenverteilung.py
+++ notenverteilung.py
@@ -1,20 +1,34 @@
-import matplotlib
+
 import matplotlib.pyplot as plt
 import csv
 
-with open('Files/schueler_noten.csv') as csvdatei:
-    notenliste = csv.reader(csvdatei)
-    for row in notenliste:
-        print(row[1])
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+def read_csv(file_path):
+    noten_anzahl =[]
+    noten_1_6 = [1, 2, 3, 4, 5, 6]
+      #open csv file
+    with open(file_path) as csvdatei:
+        klassenliste = csv.reader(csvdatei)
+        noten_str = [row[1]for row in klassenliste]  # sort out names
+    noten = [int(n) for n in noten_str if is_number(n)]  #convert to int
+    for i in range(1, 7):
+        anzahl = noten.count(i)  # count grades
+        noten_anzahl.append(anzahl)  # create list of counted grades
+    return noten_anzahl
 
 
-def plot_notenverteilung():
+def plot_notenverteilung(noten_anzahl):
     noten = [1, 2, 3, 4, 5, 6]
-    noten_anzahl = [4, 7, 10, 2, 1, 0]
     plt.bar(noten, noten_anzahl)
     plt.title('Notenverteilung')
     plt.ylabel('Anzahl')
     plt.xlabel('Noten')
     plt.show()
 
+plot_notenverteilung(read_csv('Files/schueler_noten.csv'))
 
