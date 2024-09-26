400ed430926feb3c84e0fc5833a3febf455105fc
ChrisLoehner <c.loehner410@gmail.com>
2024-09-26 19:06:08 +0200

    Durchschnitt hinzugefügt

--- notenverteilung.py
+++ notenverteilung.py
@@ -2,22 +2,30 @@
 import matplotlib.pyplot as plt
 import csv
 
+
 def is_number(s):
     try:
         int(s)
         return True
     except ValueError:
         return False
+
+
 def read_csv(file_path):
-    noten_anzahl =[]
-      #open csv file
+    noten_anzahl = []
+    # open csv file
     with open(file_path) as csvdatei:
         klassenliste = csv.reader(csvdatei)
         noten_str = [row[1]for row in klassenliste]  # sort out names
-    noten = [int(n) for n in noten_str if is_number(n)]  #convert to int
+    noten = [int(n) for n in noten_str if is_number(n)]  # convert to int
     for i in range(1, 7):
         anzahl = noten.count(i)  # count grades
         noten_anzahl.append(anzahl)  # create list of counted grades
+    # Durchschnitt berechnen
+    gesamtnote = sum(noten)
+    gesamtnotenanzahl = sum(noten_anzahl)
+    notendurchscnitt = gesamtnote / gesamtnotenanzahl
+    print(notendurchscnitt)
     return noten_anzahl
 
 
@@ -30,5 +38,5 @@ def plot_notenverteilung(noten_anzahl):
     plt.savefig('Files/Notenverteilung_Bargraph.png')
     plt.show()
 
-plot_notenverteilung(read_csv('Files/schueler_noten.csv'))
 
+plot_notenverteilung(read_csv('Files/schueler_noten.csv'))
